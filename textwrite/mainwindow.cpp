1202da98409b458a36909a16c61364afaf3055b4
Dlprogrammmer <1656892276@qq.com>
2023-06-12 23:16:54 +0800

    version1.4

---
+++ textwrite/mainwindow.cpp
@@ -0,0 +1,100 @@
+﻿#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include<qstringlist.h>
+#include<qdebug.h>
+#include<qfile.h>
+#include<qdebug.h>
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    QStringList header;
+    header<<"id";
+    header<<"name";
+    header<<"score";
+
+
+
+
+    ui->tableWidget->setColumnCount(3);
+    ui->tableWidget->setHorizontalHeaderLabels(header);
+    ui->tableWidget->setEditTriggers(QAbstractItemView::DoubleClicked | QAbstractItemView::SelectedClicked);
+    num=0;
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_add_button_clicked()
+{
+    ui->tableWidget->insertRow(num);
+    num++;
+}
+
+void MainWindow::on_delete_button_clicked()
+{
+    QList<QTableWidgetItem *> selected = ui->tableWidget->selectedItems();
+    if (!selected.isEmpty())
+    {
+        int row = selected.at(0)->row();
+        ui->tableWidget->removeRow(row);
+        num--;
+    }
+}
+
+void MainWindow::on_write_clicked()
+{
+
+    QFile file("data.dat");
+        if (!file.open(QIODevice::WriteOnly))
+            return;
+
+        QDataStream out(&file);
+        out.setVersion(QDataStream::Qt_5_9);
+
+        out << (quint32)ui->tableWidget->rowCount();
+        out << (quint32)ui->tableWidget->columnCount();
+
+        for (int row = 0; row < ui->tableWidget->rowCount(); ++row) {
+            for (int column = 0; column < ui->tableWidget->columnCount(); ++column) {
+                QTableWidgetItem* item = ui->tableWidget->item(row, column);
+                out << item->text();
+            }
+        }
+
+        file.close();
+}
+
+void MainWindow::on_read_clicked()
+{
+    QFile file("data.dat");
+        if (!file.open(QIODevice::ReadOnly))
+            return;
+
+        QDataStream in(&file);
+        in.setVersion(QDataStream::Qt_5_9);
+
+        quint32 rowCount;
+        quint32 columnCount;
+        in >> rowCount;
+        in >> columnCount;
+
+        ui->tableWidget->setRowCount(rowCount);
+        ui->tableWidget->setColumnCount(columnCount);
+
+        for (int row = 0; row < rowCount; ++row) {
+            for (int column = 0; column < columnCount; ++column) {
+                QString text;
+                in >> text;
+                QTableWidgetItem* item = new QTableWidgetItem(text);
+                ui->tableWidget->setItem(row, column, item);
+            }
+        }
+
+        file.close();
+
+}
---
+++ textwrite/mainwindow.h
@@ -0,0 +1,32 @@
+﻿#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include<qfile.h>
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void on_add_button_clicked();
+
+    void on_delete_button_clicked();
+
+    void on_write_clicked();
+
+    void on_read_clicked();
+
+private:
+    Ui::MainWindow *ui;
+    int num;
+    QFile files;
+};
+#endif // MAINWINDOW_H
